1a72c06a68b1e91d51a08fb7250e0781f3825797
Blake <112503461+bgirdler16@users.noreply.github.com>
2022-11-10 11:39:22 -0700

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "accounts.apps.AccountsConfig",
-    "crispy_forms",
+    "crispy_forms", 
 ]
 
 MIDDLEWARE = [
