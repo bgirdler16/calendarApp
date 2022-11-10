536c9aecd05084b6ab912938d0a789d8e8b33f9d
FrostyFeet7 <tyzer.clark@umconnect.umt.edu>
2022-11-10 10:57:32 -0700

    added to the accounts admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -13,6 +13,13 @@ class CustomUserAdmin(UserAdmin):
     list_display = [
         "email",
         "username",
+        "first_name"
+        "last_name"
         "age",
         "is_staff",
     ]
+    fieldsets = UserAdmin.fieldsets + ((non, {"fields": ("age",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((non, {"fields": ("age",)}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
